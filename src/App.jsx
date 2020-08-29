d901f77e25d69c00ed45357bef531b6bc69fb81e
= <=>
2020-08-29 16:09:56 +0200

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,98 @@
+import React from 'react';
+import { Redirect, Route } from 'react-router-dom';
+import {
+  IonApp,
+  IonIcon,
+  IonLabel,
+  IonRouterOutlet,
+  IonTabBar,
+  IonTabButton,
+  IonTabs
+} from '@ionic/react';
+import { IonReactRouter } from '@ionic/react-router';
+import { ellipse, square, triangle } from 'ionicons/icons';
+import Tab1 from './pages/Tab1';
+import Tab2 from './pages/Tab2';
+import Tab3 from './pages/Tab3';
+
+/* Core CSS required for Ionic components to work properly */
+import '@ionic/react/css/core.css';
+
+/* Basic CSS for apps built with Ionic */
+import '@ionic/react/css/normalize.css';
+import '@ionic/react/css/structure.css';
+import '@ionic/react/css/typography.css';
+
+/* Optional CSS utils that can be commented out */
+import '@ionic/react/css/padding.css';
+import '@ionic/react/css/float-elements.css';
+import '@ionic/react/css/text-alignment.css';
+import '@ionic/react/css/text-transformation.css';
+import '@ionic/react/css/flex-utils.css';
+import '@ionic/react/css/display.css';
+
+/* Theme variables */
+import './theme/variables.css';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    console.log('CLEVERPUSH CORDOVA EXAMPLE');
+
+    var notificationReceivedCallback = function(data) {
+      console.log('notificationReceivedCallback:', JSON.stringify(data));
+    };
+
+    var notificationOpenedCallback = function(data) {
+      console.log('notificationOpenedCallback:', JSON.stringify(data));
+    };
+
+    var subscribedCallback = (subscriptionId) => {
+      console.log('subscribedCallback  subscriptionId:', subscriptionId);
+    };
+
+    if (window.plugins) {
+      window.plugins.CleverPush.init("7R8nkAxtrY5wy5TsS", notificationReceivedCallback, notificationOpenedCallback, subscribedCallback);
+
+    }
+
+    this.state = {
+      subscribed : false
+    };
+   
+  }
+
+  render() {
+    return (
+      <IonApp>
+        <IonReactRouter>
+          <IonTabs>
+            <IonRouterOutlet>
+              <Route path="/tab1" component={Tab1} exact={true} />
+              <Route path="/tab2" component={Tab2} exact={true} />
+              <Route path="/tab3" component={Tab3} />
+              <Route path="/" render={() => <Redirect to="/tab1" />} exact={true} />
+            </IonRouterOutlet>
+            <IonTabBar slot="bottom">
+              <IonTabButton tab="tab1" href="/tab1">
+                <IonIcon icon={triangle} />
+                <IonLabel>{this.state.subscribed ? 'Subscribed' : 'Not Subscribed'}</IonLabel>
+              </IonTabButton>
+              <IonTabButton tab="tab2" href="/tab2">
+                <IonIcon icon={ellipse} />
+                <IonLabel>Tab 2</IonLabel>
+              </IonTabButton>
+              <IonTabButton tab="tab3" href="/tab3">
+                <IonIcon icon={square} />
+                <IonLabel>Tab 3</IonLabel>
+              </IonTabButton>
+            </IonTabBar>
+          </IonTabs>
+        </IonReactRouter>
+      </IonApp>
+    );
+  }
+}
+
+export default App;
